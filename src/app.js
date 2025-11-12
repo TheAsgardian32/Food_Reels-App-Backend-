d77d5a8cba80dd48eed02514c968f4d93e6478f3
TheAsgardian32 <fazilfarhan24@gmail.com>
2025-11-12 20:15:39 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -1,41 +1,46 @@
 //create a server
-const express=require('express')
-const cookieParser=require('cookie-parser')
-const mongoose=require("mongoose")
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const mongoose = require("mongoose")
+const cors = require("cors")
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-const cors=require("cors")
 
-function connectDb(){
-    mongoose.connect("mongodb+srv://fazilfarhan24_db_user:0MR3MaFs4LTX6Hv7@cluster0.roiwdyo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(()=>{console.log("Mongo connected")})
-    .catch((error)=>{
-        console.log("Error found"+error)
-    })
+
+async function connectDb() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000, // optional: prevents long waits
+    });
+        console.log("✅ MongoDB connected successfully");
+    }
+    catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1); // ⚠️ ensures app stops if DB connection fails
+  }
+    
 }
+connectDb();
 
+const authRoutes = require("./routes/auth.routes")
+const foodRoutes = require("./routes/fooditems.routes")
+const profileRoutes = require("./routes/profile.routes")
 
-// connectDb();
-
-const authRoutes=require("./routes/auth.routes")
-const foodRoutes=require("./routes/fooditems.routes")
-const profileRoutes=require("./routes/profile.routes")
-
-const app=express()
+const app = express()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://food-reels-app-frontend.vercel.app",
-    credentials:true
+    origin: "https://food-reels-app-frontend.vercel.app",
+    credentials: true
 }))
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("HelloWorld")
 })
 
-app.use("/api/auth",authRoutes) //Prefix and what happens in the route of prefix
-app.use("/api/food",foodRoutes)
-app.use("/api/profile",profileRoutes)
+app.use("/api/auth", authRoutes) //Prefix and what happens in the route of prefix
+app.use("/api/food", foodRoutes)
+app.use("/api/profile", profileRoutes)
 
-module.exports=app;
+module.exports = app;
