fff014cff4d0aed3010370e86764a81d07e26fdd
TheAsgardian32 <fazilfarhan24@gmail.com>
2025-11-13 16:19:29 +0530

    new born backend

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 //This is to start a server
 const app=require("./src/app")
-const connectDb=require("./src/db/db")
-
-
+const mongoose=require("mongoose")
+// const connectDb=require("./src/db/db")
+const port=process.env.PORT || 3000
+async function connectDb() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000, // optional: prevents long waits
+    });
+        console.log("✅ MongoDB connected successfully");
+    }
+    catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1); // ⚠️ ensures app stops if DB connection fails
+  }
+    
+}
 connectDb();
 
-
-app.listen(3000,()=>{
-    console.log("App listening on port 3000")
+app.listen(port,()=>{
+    console.log("App listening on port ")
 })
 
 
--- src/app.js
+++ src/app.js
@@ -3,38 +3,23 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const mongoose = require("mongoose")
 const cors = require("cors")
+const app = express()
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    // origin: "https://food-reels-app-frontend.vercel.app",
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-
-
-async function connectDb() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 10000, // optional: prevents long waits
-    });
-        console.log("✅ MongoDB connected successfully");
-    }
-    catch (error) {
-    console.error("❌ MongoDB connection error:", error);
-    process.exit(1); // ⚠️ ensures app stops if DB connection fails
-  }
-    
-}
-connectDb();
 
 const authRoutes = require("./routes/auth.routes")
 const foodRoutes = require("./routes/fooditems.routes")
 const profileRoutes = require("./routes/profile.routes")
 
-const app = express()
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-    origin: "https://food-reels-app-frontend.vercel.app",
-    // origin: "http://localhost:5173",
-    credentials: true
-}))
+
 
 app.get("/", (req, res) => {
     res.send("HelloWorld")
@@ -43,5 +28,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes) //Prefix and what happens in the route of prefix
 app.use("/api/food", foodRoutes)
 app.use("/api/profile", profileRoutes)
+
 
 module.exports = app;
